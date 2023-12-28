1e652476fb8b5158cd037db2ceb76eb529370ec6
hamayou-61 <caster2596@gmail.com>
2023-12-28 21:28:25 +0900

    update astro4

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,5 +1,7 @@
 //SDK利用準備
-import { createClient, MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+import type {MicroCMSQueries } from "microcms-js-sdk";
+
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
